d619943cd18c51885b407b25b9d5fea1907e6362
Bach16 <gwyn56717@gmail.com>
2025-06-01 18:43:53 -0500

    change image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -389,8 +389,12 @@ const Home = () => {
           <div className="grid md:grid-cols-4 gap-8">
             <div className="space-y-4">
               <div className="flex items-center space-x-3">
-                <div className="w-10 h-10 bg-gradient-to-br from-blue-500 to-purple-600 rounded-lg flex items-center justify-center">
-                  <MapPin className="w-6 h-6 text-white" />
+                <div className="w-10 h-10  flex items-center justify-center">
+                  <img
+                    src="logo-03.png"
+                    alt="WalkARCoin Features"
+                    className="w-full h-auto object-cover "
+                  />
                 </div>
                 <div>
                   <h3 className="text-xl font-bold">WARC</h3>
